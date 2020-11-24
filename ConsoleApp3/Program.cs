e25e0b456723a9b2a43f3ff62133e89ce770e240
saew <saew0163@gmail.com>
2020-11-24 10:43:20 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi, " + name + ", have a nice day! ^_^");
+        }
+    }
+}
